4888352706c760c8863bc9cfa75cee992d4c10d0
nareiqat <areiqat2@gmail.com>
2020-12-31 20:42:58 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+const api = {
+  key: "e50f480a3db99ee7cf9505826a51a31a",
+  base: "http://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <div className='search-box'>
+          <input type ='text'
+          className='search-bar'
+          placeholder='search...'
+          />
+        </div>
+      </main>
+      
     </div>
   );
 }
